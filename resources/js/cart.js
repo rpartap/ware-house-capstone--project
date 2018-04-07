a6a55c68941a92ef00efe8d1a49bf1230c7124ea
Regina Partap <regina_partap@yahoo.com>
2018-04-07 16:19:30 -0400

    Finish responsiveness

--- resources/js/cart.js
+++
@@ -1,7 +0,0 @@
-var listItemHTML = '<div id="item' + i + '"><input type="checkbox" onclick="crossedOut(\'item' + i + '\');">' + listItem +
-'<input type = "button" value="Remove" onclick="removeItem(\'item' + i + '\');"></div>';
-
-function removeItem(item){
-        var itemToRemove = document.getElementById(item);
-        itemToRemove.parentNode.removeChild(itemToRemove);
-}
